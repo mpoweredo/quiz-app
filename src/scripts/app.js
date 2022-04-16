8c5b14083d592fa56cd35b92af76a069b61b316e
mpoweredo <mpow3r1337@gmail.com>
2022-04-16 16:30:29 +0200

    styles and split functions into files

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,77 +1,23 @@
-import { quizes } from "./quizes.js";
 import { renderQuizes } from "./selectQuiz.js";
 
-
-const quizTitle = document.getElementById('quiz-title');
-const startGameBtn = document.getElementById('start-btn');
-const startGameContainer = document.querySelector('.start-game')
-// const nextQuestionbtn = document.getElementById('next-question')
-const quiz = document.getElementById('quiz')
-const question = document.getElementById('question')
-const questionsContainer = document.getElementById('questions')
-const result = document.getElementById('result')
-let CURRENT_QUESTION = 0;
-let CURRENT_QUIZ;
-let points = 0;
-
-
-
-export const startGame = (selectedQuiz) => {
-    startGameContainer.classList.add('hide')
-    quiz.classList.remove('hide')
-    renderQuestion(quizes[selectedQuiz]);
+export const quizHandler = {
+    CURRENT_QUESTION: 0,
+    CURRENT_QUIZ: 0,
+    points: 0,
 }
-
-startGameBtn.addEventListener('click', startGame)
-
-const renderQuestion = quiz => {
-    quizTitle.textContent = quiz.title;
-    question.textContent = quiz.questions[CURRENT_QUESTION].question
-    clearQuestions();
-    renderAnswers(quiz.questions[CURRENT_QUESTION])
-    CURRENT_QUIZ = quiz;
-}
-
-const renderAnswers = question => {
+export const questionsContainer = document.getElementById('questions');
 
 
-    for (const questionEl of question.answers) {
-        const answerButton = document.createElement('button')
-        answerButton.textContent = questionEl.text;
-        answerButton.dataset.answer = questionEl.correct;
-        questionsContainer.appendChild(answerButton)
-        answerButton.addEventListener('click', checkAnswer)
-    }
-}
-
-const clearQuestions = () => {
-    while (questionsContainer.lastElementChild) {
-        questionsContainer.lastElementChild.remove();
-    }
-}
-
-const checkAnswer = e => {
-    const button = e.target;
 
 
-    if (button.dataset.answer === 'true') {
-        points++
-        nextQuestion();
-    } else {
-        nextQuestion();
-    }
-}
 
 
-const nextQuestion = () => {
-    // console.log(CURRENT_QUIZ.questions.length)
-    // console.log(CURRENT_QUESTION)
-    CURRENT_QUESTION++
-    if (CURRENT_QUIZ.questions.length <= CURRENT_QUESTION) {
-        quiz.classList.add('hide')
-        result.parentNode.classList.remove('hide')
-        result.textContent = `ukonczyles quiz z wynikiem ${points}/${CURRENT_QUIZ.questions.length} punktow!`
-        return
-    }
-    renderQuestion(CURRENT_QUIZ);
-}
+
+
+
+
+
+
+
+
+
